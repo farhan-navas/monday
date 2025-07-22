589925579e0a9d7d63e3fc3f82f8a784777077bd
farhan-navas <farhanmnavas@gmail.com>
2025-07-23 03:31:46 +0800

    Add sirikit listener in main, compartmentalize later

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,63 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:uni_links/uni_links.dart';
 import 'pages/weather_page.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  StreamSubscription<Uri?>? _sub;
+
+  @override
+  void initState() {
+    super.initState();
+    _handleInitialUri();
+    _sub = uriLinkStream.listen((Uri? uri) {
+      if (uri != null) _processUri(uri);
+    });
+  }
+
+  Future<void> _handleInitialUri() async {
+    try {
+      final initialUri = await getInitialLink();
+
+      if (initialUri != null) _processUri(initialUri as Uri);
+    } on FormatException {
+      print("URI Format error, somthing is wrong oops");
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  void _processUri(Uri uriString) {
+    final uri = Uri.parse(uriString as String);
+    if (uri.host == 'weather' && uri.queryParameters['city'] != null) {
+      final city = uri.queryParameters['city']!;
+      print('🔔 Siri wants weather for: $city');
+      // TODO: wire into WeatherPage via maybe  a provider or setState(?)
+    }
+  }
+
+  @override
+  void dispose() {
+    _sub?.cancel();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: true,
+      title: 'M.O.N.D.A.Y.',
       home: WeatherPage(),
     );
   }
--- lib/pages/weather_page.dart
+++ lib/pages/weather_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:lottie/lottie.dart';
 import 'package:monday/models/weather_model.dart';
 import 'package:monday/services/weather_service.dart';
@@ -15,7 +16,7 @@ class _WeatherPageState extends State<WeatherPage> {
   final _weatherService = WeatherService('39aa2ccd23b42316e8d41a9690d9a931');
   WeatherModel? _weatherModel;
 
-  _fetchWeather() async {
+  void _fetchWeather() async {
     String cityName = await _weatherService.getCurrentCity();
 
     try {
@@ -61,21 +62,35 @@ class _WeatherPageState extends State<WeatherPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
+      // backgroundColor: Colors.white,
+      body: SafeArea(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(_weatherModel?.cityName ?? "Loading city.."),
-
-            Lottie.asset(
-              'assets/${getWeatherAnimation(_weatherModel?.mainCondition)}',
+            const SizedBox(height: 24),
+            Text(
+              _weatherModel?.cityName ?? "Loading city...",
+              style: GoogleFonts.poppins(
+                fontSize: 28,
+                fontWeight: FontWeight.w600,
+              ),
             ),
-
-            Text('${_weatherModel?.temperature.round()}°C'),
-            ElevatedButton(
-              onPressed: _fetchWeather,
-              child: const Text('Refresh'),
+            const SizedBox(height: 24),
+            Expanded(
+              child: Lottie.asset(
+                'assets/${getWeatherAnimation(_weatherModel?.mainCondition)}',
+                height: 300,
+              ),
             ),
+            Text(
+              _weatherModel != null
+                  ? '${_weatherModel!.temperature.round()}°C'
+                  : '',
+              style: GoogleFonts.poppins(
+                fontSize: 24,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+            const SizedBox(height: 48),
           ],
         ),
       ),
